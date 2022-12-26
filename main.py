e0ef57e8b6540de04566726a163f7fecc618b8f7
yugamdeep <120464917+yugamdeep@users.noreply.github.com>
2022-12-26 15:58:53 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+import time
+
+#VARIABLES
+hour = int(time.strftime("%H"))
+minute =int(time.strftime("%M"))
+second = int(time.strftime("%S"))
+print("The time right now is =>",hour-12,":",minute,":",second)
